6d23e700cce542f40d83b52dd2a17b3138bce761
Juan Francisco <Juan Francisco@JuanFrancisco>
2017-01-18 16:32:46 +0100

    Commit clases

--- src/proyectopf/ProyectoPF.java
+++ src/proyectopf/ProyectoPF.java
@@ -15,7 +15,10 @@ public class ProyectoPF {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        
+        Ventana v = new Ventana();
+        v.setVisible(true);
     }
     
 }
