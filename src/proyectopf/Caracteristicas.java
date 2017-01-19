6655e3118b6f2a8f6f717aafb0da15373eabc2a7
Juan Francisco <Juan Francisco@JuanFrancisco>
2017-01-19 17:40:27 +0100

    Commit SVA ok

--- src/proyectopf/Caracteristicas.java
+++ src/proyectopf/Caracteristicas.java
@@ -11,4 +11,46 @@ package proyectopf;
  */
 public class Caracteristicas {
     
+    private double factor_Ajuste; //Factor de ajuste
+    private int sva; //Suma de los valores de los atributos
+    private double pf; //Punto funcion
+
+    public Caracteristicas() {
+        this.factor_Ajuste = 0;
+        this.sva = 0;
+        this.pf = 0;
+    }
+  
+    public double getFactor_Ajuste() {
+        return factor_Ajuste;
+    }
+
+    public void setFactor_Ajuste(double factor_Ajuste) {
+        this.factor_Ajuste = factor_Ajuste;
+    }
+
+    public int getSva() {
+        return sva;
+    }
+
+    public void setSva(int sva) {
+        this.sva = sva;
+    }
+    
+    public double calculoFactorAjuste(){
+        return this.factor_Ajuste= 0.65 + (0.01 * this.getSva());
+    }
+    
+    //Punto funcion no ajustado (PFNA)
+    public double Punto_Funcion_Ajustado(double PFNA) {
+        return this.pf = PFNA * this.getFactor_Ajuste();
+    }
+
+    public double getPf() {
+        return pf;
+    }
+
+    public void setPf(double pf) {
+        this.pf = pf;
+    }
 }
