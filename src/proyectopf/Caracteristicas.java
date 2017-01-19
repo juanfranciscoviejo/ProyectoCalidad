2b6a511b46652a623e2e6bb9b08fa090c39aaeb9
Horizon <Horizon@LAPTOP-Horizon>
2017-01-19 18:16:30 +0100

    commint eliminado metodo para calcular los puntos funcion

--- src/proyectopf/Caracteristicas.java
+++ src/proyectopf/Caracteristicas.java
@@ -10,5 +10,32 @@ package proyectopf;
  * @author Juan Francisco
  */
 public class Caracteristicas {
+    private double factor_Ajuste; //Factor de ajuste
+    private int sva; //Suma de los valores de los atributos
+
+    public Caracteristicas() {
+        this.factor_Ajuste = 0;
+        this.sva = 0;
+    }
+  
+    public double getFactor_Ajuste() {
+        return factor_Ajuste;
+    }
+
+    public void setFactor_Ajuste(double factor_Ajuste) {
+        this.factor_Ajuste = factor_Ajuste;
+    }
+
+    public int getSva() {
+        return sva;
+    }
+
+    public void setSva(int sva) {
+        this.sva = sva;
+    }
     
+    public double calculoFactorAjuste(){
+        return this.factor_Ajuste= 0.65 + (0.01 * this.getSva());
+    }
+
 }
