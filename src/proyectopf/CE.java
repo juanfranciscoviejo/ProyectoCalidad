b5d58eeb2c6ddd5e164226542e58f2fb1bc15ddd
carlo <carlo@192.168.1.37>
2017-01-20 01:45:57 +0100

    Clase CE

---
+++ src/proyectopf/CE.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopf;
+
+/**
+ *
+ * @author Carlos
+ */
+public class CE extends Transacciones {
+
+    private int fS;
+    private int eS;
+    
+    public CE(String d, int f, int e, int fS, int eS) {
+        super(d, f, e);
+        this.fS = fS;
+        this.eS = eS;
+    }
+
+    public String complejidad_S() {
+        String s = "";
+        if (eS <= 5 && fS <= 3) {
+            s = "S";
+        } else if (eS <= 5 && fS >= 4) {
+            s = "M";
+        } else if (eS >= 6 && eS <= 19 && fS < 2) {
+            s = "S";
+        } else if (eS >= 6 && (eS <= 19 && fS == 2 || fS == 3) ) {
+            s = "M";
+        } else if (eS >= 6 && eS <= 19 && fS >= 4) {
+            s = "C";
+        } else if (eS >= 20 && fS <= 1) {
+            s = "M";
+        } else if (eS >= 20 && fS >2) {
+            s = "C";
+        } 
+        return s;
+    }
+    
+    public String complejidad() {
+        String s = "";
+        if (getElementales() <= 4 && getFicheros() <= 2) {
+            s = "S";
+        } else if (getElementales() <= 4 && getFicheros() >= 3) {
+            s = "M";
+        } else if (getElementales() >= 5 && getElementales() <= 15 && getFicheros() < 2) {
+            s = "S";
+        } else if (getElementales() >= 5 && getElementales() <= 15 && getFicheros() == 2) {
+            s = "M";
+        } else if (getElementales() >= 5 && getElementales() <= 15 && getFicheros() > 2) {
+            s = "C";
+        } else if (getElementales() >= 16 && getFicheros() <= 1) {
+            s = "M";
+        } else if (getElementales() >= 16 && getFicheros() == 2) {
+            s = "C";
+        } else if (getElementales() >= 16 && getFicheros() >= 3) {
+            s = "C";
+        }
+        
+        String a = complejidad_aux();
+        if (s=="S" && a!="S"){
+            s = a;
+        } else if (s=="M" && a=="C"){
+            s = a;
+        }
+        
+        return s;
+    }
+
+}
